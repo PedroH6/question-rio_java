79b712711e228a75239c49d604f09a6379190157
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-10-23 16:46:02 -0300

    while

--- src/estruturarepeticao/EstruturaRepeticao.java
+++ src/estruturarepeticao/EstruturaRepeticao.java
@@ -4,33 +4,53 @@ import java.util.Scanner;
 
 public class EstruturaRepeticao {
     public  static void main(String[] args) {
-        System.out.println("Se você deseja ver o for crescente, digite 1:\nSe deseja ver o for decrencente, digite 2: ");
 
-        Scanner sc = new Scanner(System.in);
-        int decisao = sc.nextInt();
 
-        if (decisao == 1){
-            forCrencente();
-        } else if (decisao == 2) {
-            forDescrencente();
-        } else {
-            System.out.println("Decição inválida: \n");
-        }
+
+
 
     }
 
     public static void forCrencente(){
-        System.out.println("FOR CRESCENTE:\n");
-        for (int n1 = 0; n1 < 101; n1++) {
-            System.out.println("O contador está em:" + n1);
+        System.out.println("Se você deseja ver o for crescente, digite 1:");
+        Scanner sc = new Scanner(System.in);
+        int decisao = sc.nextInt();
+
+        if (decisao == 1) {
+            for (int n1 = 0; n1 < 101; n1++) {
+                System.out.println("O contador está em:" + n1);
+            }
         }
         System.out.println();
     }
     public static void forDescrencente(){
-        System.out.println("FOR DECRESCENTE:\n");
+        System.out.println("Se deseja ver o for decrencente, digite 2:");
+        Scanner sc = new Scanner(System.in);
+        int decisao = sc.nextInt();
+
+        if (decisao == 2){
         for (int n1 = 100; n1 >= 0; n1--){
            System.out.println("O contador está em: " + n1);
         }
+        }
         System.out.println();
+    }
+
+    public static void whileNumeroDoUsuario() {
+        System.out.println("Digite um número maior que 30 para ser exibido. Digite 30 ou um numero menor que 30 para sair:");
+        Scanner sc = new Scanner(System.in);
+        int numeroDoUsuario = sc.nextInt();
+
+        while (numeroDoUsuario > 30) {
+            System.out.println("O número que você digitou é: " + numeroDoUsuario);
+
+            System.out.println("Digite um número maior que 30 para ser exibido. Digite 30 ou um numero menor que 30 para sair:");
+
+            numeroDoUsuario = sc.nextInt();
+
+        }
+        sc.close();
+        System.out.println("Você saiu!");
+
     }
 }
